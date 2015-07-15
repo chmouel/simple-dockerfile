d5b821a24ec91ac10020a92773c9ce0cc6b84111
Chmouel Boudjnah <chmouel@redhat.com>
2015-07-15 10:03:08 +0200

    realpath

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,2 +1,7 @@
+function readlinkf() {
+    python -c 'import os,sys;print os.path.realpath(sys.argv[1])' $1
+}
+cd "$(dirname $(readlinkf $0/../))/fragments"
+
 oc delete all --all
 for i in *yaml;do oc create -f $i;done;oc start-build simple-test
--- scripts/reattach.sh
+++ scripts/reattach.sh
@@ -1,1 +1,7 @@
+function readlinkf() {
+    python -c 'import os,sys;print os.path.realpath(sys.argv[1])' $1
+}
+cd "$(dirname $(readlinkf $0/../))/fragments"
+
+
 oc exec -p $(oc get pod|grep Running|grep -E 'simple-test-[0-9]'|awk '{print $1}') -i -t -- bash -il
